ae7261f6e6a076855146b30c7f675fdfda499217
rajput-karsh <utkarsh1@appinventiv.com>
2024-03-30 15:28:35 +0530

    add bubbles

---
+++ src/components/bubble/index.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useRef } from 'react';
+
+function Bubble() {
+  const bubbleRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const BubbleAnimationEffect = function () {
+      bubbleRef.current &&
+        bubbleRef.current.animate(
+          {
+            borderTopLeftRadius: String(
+              Math.round(Math.random() * 800 + 350) + 'px'
+            ),
+            borderTopRightRadius: String(
+              Math.round(Math.random() * 800 + 350) + 'px'
+            ),
+            borderBottomLeftRadius: String(
+              Math.round(Math.random() * 800 + 350) + 'px'
+            ),
+            borderBottomRightRadius: String(
+              Math.round(Math.random() * 800 + 350) + 'px'
+            ),
+          },
+          50000
+        );
+    };
+
+    BubbleAnimationEffect();
+
+    const BgColor = [
+      '#f75774',
+      '#f76580',
+      '#f7748c',
+      '#ed8598',
+      '#ed8598',
+      '#f7748c',
+      '#f76580',
+      '#f75774',
+    ];
+    let i = 0;
+    const BubbleEffect = function () {
+      if (i < BgColor.length && bubbleRef.current) {
+        bubbleRef.current.style['backgroundColor'] = BgColor[i];
+      } else {
+        i = 0;
+      }
+      i++;
+    };
+    setInterval(BubbleEffect, 3000);
+  }, []);
+
+  return <div ref={bubbleRef} className="bubble-circle"></div>;
+}
+
+export default Bubble;
--- src/components/uploader/index.tsx
+++ src/components/uploader/index.tsx
@@ -1,37 +1,51 @@
-import { ChangeEvent, Dispatch, useRef } from "react";
+import { ChangeEvent, Dispatch, useRef } from 'react';
 import addImg from '../../assets/add.svg';
 import css from './index.module.scss';
+import Bubble from '../bubble';
 
 interface UploaderProps {
   setImage: Dispatch<File>;
 }
 
 function Uploader({ setImage }: UploaderProps) {
-
   const imageRef = useRef<HTMLInputElement>(null);
   const handleImage = (e: ChangeEvent<HTMLInputElement>) => {
     const image = e.target.files?.[0];
     image && setImage(image);
   };
 
+  const renderBubbles = () => {
+    const data = [];
+    for (let i = 0; i < 5; i++) {
+      data.push(
+        <div className={css.imageContainer} key={crypto.randomUUID()}>
+          <Bubble />
+        </div>
+      );
+    }
+
+    return data;
+  };
 
   return (
     <div className={css.uploader}>
-      <label></label>
-      <input
-        ref={imageRef}
-        type="file"
-        accept="image/*"
-        className={css.fileHandler}
-        onChange={handleImage}
-      />
-      <img
-        src={addImg}
-        className={css.addImg}
-        onClick={() => {
-          imageRef.current?.click();
-        }}
-      />
+      {renderBubbles()}
+      <div className={css.imageContainer}>
+        <input
+          ref={imageRef}
+          type="file"
+          accept="image/*"
+          className={css.fileHandler}
+          onChange={handleImage}
+        />
+        <img
+          src={addImg}
+          className={css.addImg}
+          onClick={() => {
+            imageRef.current?.click();
+          }}
+        />
+      </div>
     </div>
   );
 }
