dfd6a6fb5966dc87895dae897fa9b3b2a634b615
utkarsh <r.utkarsh.0010@gmail.com>
2024-04-05 03:32:45 +0530

    WIP: Customization box

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import ReactCompareImage from 'react-compare-image';
 
 import css from './index.module.scss';
+import Customization from '../customization';
 interface EditorProps {
   image: string;
 }
@@ -10,7 +11,13 @@ function Editor({ image }: EditorProps) {
   const [editedImage, setEditedImage] = useState<string>(image);
   return (
     <div className={css.editorRoot}>
-      <ReactCompareImage leftImage={image} rightImage={editedImage} />
+      <ReactCompareImage
+        leftImage={image}
+        sliderLineColor="rgb(0 0 0 / 0.6)"
+        sliderLineWidth={5}
+        rightImage={editedImage}
+      />
+      <Customization side={'right'} image={editedImage} setImage={setEditedImage} />
     </div>
   );
 }
